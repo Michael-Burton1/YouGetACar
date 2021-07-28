ede5774d4cd8539838edb6588c09c9ae05a8b89b
Michael Burton <micbur1@gmail.com>
2021-07-28 09:41:58 -0700

    add black box code to be able to run program

---
+++ ClientSide/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Drawings from './Drawings';
+
+function App() {
+  return (
+    <React.Fragment>
+      < Drawings />
+    </React.Fragment>
+  );
+}
+
+export default App;
