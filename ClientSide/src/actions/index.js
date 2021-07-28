f17c2c84697a2e818f7087786cd22906734f1fc2
Michael Burton <micbur1@gmail.com>
2021-07-28 09:30:33 -0700

    add to the index action to make the API call

---
+++ ClientSide/src/actions/index.js
@@ -0,0 +1,30 @@
+import * as c from './ActionTypes';
+
+export const requestDrawings = () => ({
+  type: c.REQUEST_DRAWINGS
+});
+
+export const getDrawingsSuccess = (drawings) => ({
+  type: c.GET_DRAWINGS_SUCCESS,
+  drawings
+});
+
+export const getDrawingsFailure = (error) => ({
+  type: c.GET_DRAWINGS_FAILURE,
+  error
+});
+
+export const makeApiCall = () => {
+  return dispatch => {
+    dispatch(requestDrawings);
+    return fetch(`https://localhost:5051/api/Drawings`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          dispatch(getDrawingsSuccess(jsonifiedResponse.results));
+        })
+      .catch((error) => {
+        dispatch(getDrawingsFailure(error));
+      });
+  }
+}
